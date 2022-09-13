9194a64318cd6d170324917714ccea2454c65d81
Juan Carlos <jc.campbellg@gmail.com>
2022-09-13 14:24:37 -0600

    add index widget

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,21 @@
+import moment from 'moment'
 import Head from 'next/head'
 
 export default function Home() {
+  const today = moment().format('MMMM Do YYYY');
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>JC's Life Notion Widgets</title>
         <meta name="description" content="Notion widgets for me" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome Juan Carlos!
+      <main>
+        <h1 className='text-lg'>
+          Welcome <span className='font-bold'>Juan Carlos!</span>
+        </h1>
+        <h1 className='text-lg'>
+          Today is <span className='font-bold'>{today}</span>
         </h1>
       </main>
     </div>
